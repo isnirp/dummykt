33dfad5e66b164a1f07cfc641eae5295f060db5d
isnirp <pramankwah@gmail.com>
2023-06-13 16:15:04 +0200

    catch exceptions

--- dummydns/src/test/kotlin/com/flimbis/dummydns/DnsServiceTest.kt
+++ dummydns/src/test/kotlin/com/flimbis/dummydns/DnsServiceTest.kt
@@ -5,8 +5,9 @@ import com.flimbis.dummydns.model.DnsRecordDto
 import com.flimbis.dummydns.model.QType
 import io.mockk.every
 import io.mockk.mockk
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.lang.RuntimeException
 
 
 class DnsServiceTest {
@@ -35,6 +36,28 @@ class DnsServiceTest {
         val result = service.getRecord(qType)
 
         assertThat(result).isNotNull()
+    }
+
+    @Test
+    fun `throw exception when qType is invalid`() {
+        val qType = "NX"
+        every { validator.isValidQType(qType) } returns false
+
+        assertThatThrownBy{ service.getRecord(qType) }
+            .isInstanceOf(RuntimeException::class.java)
+            .hasMessage("not found type")
+    }
+
+    @Test
+    fun `throw exception when record not found`() {
+        val qType = "CNAME"
+        records.add(DnsRecord("www.google.com", QType.NS, "google", 3000))
+
+        every { validator.isValidQType(qType) } returns true
+
+        assertThatExceptionOfType(RuntimeException::class.java)
+            .isThrownBy { service.getRecord(qType) }
+            .withMessage("not found record")
     }
 
 }
