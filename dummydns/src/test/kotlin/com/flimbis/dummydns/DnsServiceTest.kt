b516ab85d0e0173daf556a9986205329f999791a
isnirp <pramankwah@gmail.com>
2023-06-13 16:02:06 +0200

    get record

--- dummydns/src/test/kotlin/com/flimbis/dummydns/DnsServiceTest.kt
+++ dummydns/src/test/kotlin/com/flimbis/dummydns/DnsServiceTest.kt
@@ -2,6 +2,8 @@ package com.flimbis.dummydns
 
 import com.flimbis.dummydns.model.DnsRecord
 import com.flimbis.dummydns.model.DnsRecordDto
+import com.flimbis.dummydns.model.QType
+import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -15,12 +17,24 @@ class DnsServiceTest {
 
     @Test
     fun `create record`() {
-        val recordDto: DnsRecordDto = DnsRecordDto("www.google.com","CNAME","google")
+        val recordDto: DnsRecordDto = DnsRecordDto("www.google.com", "CNAME", "google")
 
         val result = service.createRecord(recordDto)
 
         assertThat(result).isNotNull()
         assertThat(records).isNotEmpty()
+    }
+
+    @Test
+    fun `get record by qType`() {
+        val qType = "CNAME"
+        records.add(DnsRecord("www.google.com", QType.CNAME, "google", 3000))
+
+        every { validator.isValidQType(qType) } returns true
+
+        val result = service.getRecord(qType)
+
+        assertThat(result).isNotNull()
     }
 
 }
